c9f40a790b80e4c78d9d77a4622b1585242ca36b
anmolss-tech <anmolss.tech@gmail.com>
2026-05-29 12:07:34 -0700

    FIXBEE-196 making collection on the db

--- utilities/populatingDB/seed.js
+++ utilities/populatingDB/seed.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
-
 let MongoClient = require('mongodb').MongoClient;
+let dbName = process.env.DB_NAME;
+
+
 
 async function run() {
   let client = new MongoClient(process.env.MONGODB_URI);
   await client.connect();
 
-  let db = client.db(process.env.DB_NAME);
-  await db.createCollection(process.env.COL_NAME);
+  let db = client.db(dbName);
+  col = db.collection(process.env.COL_NAME);
 
   await client.close();
-  console.log('setup done');
+  console.log('done');
 }
 
 run();

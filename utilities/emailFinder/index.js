f69d46ae118fe011c757ab9be60aff0ed65c30ac
anmolss-tech <anmolss.tech@gmail.com>
2026-05-31 12:53:13 -0700

    FIXBEE-197: connecting to mongoDB, and collection via .env

--- utilities/emailFinder/index.js
+++ utilities/emailFinder/index.js
@@ -1,1 +1,21 @@
-index.js
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
+main()
+
+async function main() {
+   
+    const client = new MongoClient(process.env.mongoDBURI)
+    await client.connect()  
+    await client.close()
+
+    const database = client.db(process.env.DB_NAME)
+    const providersCol = database.collection(process.env.COL_NAME)
+    const cacheCol = database.collection(process.env.COL_CACHE)
+
+    console.log('\n MongoDb closed')
+    
+
+}
+
+//FIXBEE-197

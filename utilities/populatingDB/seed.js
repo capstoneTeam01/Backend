85763988a672069508f574e546bce217a5c03261
anmolss-tech <anmolss.tech@gmail.com>
2026-05-31 01:22:13 -0700

    FIXBEE-196: enabling a lot of locations to collect the data

--- utilities/populatingDB/seed.js
+++ utilities/populatingDB/seed.js
@@ -8,20 +8,20 @@ let category = 'plumber';
 let limitPerCity = 1;
 
 let cities = [
-//   { name: 'Burnaby', lat: 49.2488, lng: -122.9805 },
-//   { name: 'Surrey', lat: 49.1913, lng: -122.8490 },
-//   { name: 'Richmond', lat: 49.1666, lng: -123.1336 },
-//   { name: 'North Vancouver', lat: 49.3199, lng: -123.0724 },
-//   { name: 'West Vancouver', lat: 49.3286, lng: -123.1602 },
-//   { name: 'Coquitlam', lat: 49.2838, lng: -122.7932 },
-//   { name: 'Port Coquitlam', lat: 49.2628, lng: -122.7811 },
-//   { name: 'New Westminster', lat: 49.2057, lng: -122.9110 },
-//   { name: 'Delta', lat: 49.0847, lng: -123.0586 },
-//   { name: 'Langley', lat: 49.1044, lng: -122.6604 },
-//   { name: 'Abbotsford', lat: 49.0504, lng: -122.3045 },
-//   { name: 'Victoria', lat: 48.4284, lng: -123.3656 },
-//   { name: 'Kelowna', lat: 49.8880, lng: -119.4960 },
-//   { name: 'Nanaimo', lat: 49.1659, lng: -123.9401 },
+  { name: 'Burnaby', lat: 49.2488, lng: -122.9805 },
+  { name: 'Surrey', lat: 49.1913, lng: -122.8490 },
+  { name: 'Richmond', lat: 49.1666, lng: -123.1336 },
+  { name: 'North Vancouver', lat: 49.3199, lng: -123.0724 },
+  { name: 'West Vancouver', lat: 49.3286, lng: -123.1602 },
+  { name: 'Coquitlam', lat: 49.2838, lng: -122.7932 },
+  { name: 'Port Coquitlam', lat: 49.2628, lng: -122.7811 },
+  { name: 'New Westminster', lat: 49.2057, lng: -122.9110 },
+  { name: 'Delta', lat: 49.0847, lng: -123.0586 },
+  { name: 'Langley', lat: 49.1044, lng: -122.6604 },
+  { name: 'Abbotsford', lat: 49.0504, lng: -122.3045 },
+  { name: 'Victoria', lat: 48.4284, lng: -123.3656 },
+  { name: 'Kelowna', lat: 49.8880, lng: -119.4960 },
+  { name: 'Nanaimo', lat: 49.1659, lng: -123.9401 },
   { name: 'Vancouver', lat: 49.2827, lng: -123.1207 }
 ];
 
@@ -41,6 +41,7 @@ async function run() {
     await foursquare(cityInfo);
     await google(cityInfo);
     await yelp(cityInfo);
+    await apify(cityInfo);
   }
 
   await client.close();

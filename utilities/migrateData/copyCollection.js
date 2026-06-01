4271b301dbaa39cd3771f217a4afa7eff06233fa
anmolss-tech <anmolss.tech@gmail.com>
2026-06-01 11:36:34 -0700

    FIXBEE199: setting up checks and main function run

--- utilities/migrateData/copyCollection.js
+++ utilities/migrateData/copyCollection.js
@@ -13,3 +13,15 @@ const col2Name = process.env.COL2;
 const dryRun = process.env.DRY_RUN === "true";
 const limitValue = process.env.LIMIT;
 const createdField = process.env.CREATED_FIELD || "createdDate";
+
+
+if (!mongo1 || !mongo2 || !db1Name || !db2Name || !col1Name || !col2Name) {
+  console.log("Missing .env values. Check mongo1, mongo2, DB1, DB2, COL1, COL2.");
+  return;
+}
+
+run();
+
+async function run() {
+
+}

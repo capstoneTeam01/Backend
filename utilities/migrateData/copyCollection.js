2d9a3dd6b1470f0fd32f7020a1ee3df78301af59
anmolss-tech <anmolss.tech@gmail.com>
2026-06-01 11:35:51 -0700

    FIXBEE199: env setup for migrateData

--- utilities/migrateData/copyCollection.js
+++ utilities/migrateData/copyCollection.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+
+
+const mongo1 = process.env.mongo1;
+const mongo2 = process.env.mongo2;
+
+const db1Name = process.env.DB1;
+const db2Name = process.env.DB2;
+
+const col1Name = process.env.COL1;
+const col2Name = process.env.COL2;
+
+const dryRun = process.env.DRY_RUN === "true";
+const limitValue = process.env.LIMIT;
+const createdField = process.env.CREATED_FIELD || "createdDate";

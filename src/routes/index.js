b9e0cf60929fd6c0bf5a0bc180f7481622b16244
anmolss-tech <anmolss.tech@gmail.com>
2026-06-05 13:03:54 -0700

    resolving conflict

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,8 @@ import userRoutes from "./userRoutes.js";
 import photoRoutes from "./photoRoutes.js";
 import providerRoutes from "./providerRoutes.js";
 import analysisRoutes from "./analysisRoutes.js";
-<<<<<<< FIXBEE-87-Setup-provider-route-in-the-backend-archtiecture
 import businessDirectoryProviderRoutes from "./businessDirectoryProviderRoutes.js";
-=======
 import notificationRoutes from "./notificationRoutes.js";
->>>>>>> pre-beta
 
 export {
   authRoutes,
@@ -15,9 +12,6 @@ export {
   photoRoutes,
   providerRoutes,
   analysisRoutes,
-<<<<<<< FIXBEE-87-Setup-provider-route-in-the-backend-archtiecture
   businessDirectoryProviderRoutes,
-=======
   notificationRoutes,
->>>>>>> pre-beta
 };

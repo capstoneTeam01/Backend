4eb7b1ccad11e3e731bf932821a669cd7a146a2a
anmolss-tech <anmolss.tech@gmail.com>
2026-06-11 20:52:09 -0700

    FIXBEE-289 printing env variables for checking

--- utilities/descriptionFinder/index.js
+++ utilities/descriptionFinder/index.js
@@ -3,3 +3,20 @@ import "dotenv/config";
 const oldCol = process.env.COL_NAME;
 const newCol = process.env.ENRICHED_COL_NAME;
 const limit = Number(process.env.LIMIT);
+
+
+run();
+
+async function run() {
+
+    if (!process.env.MONGOURI || !process.env.GROQ_API_KEY) {
+        console.log("Please add mongoDBURI and GROQ_API_KEY in .env");
+        return;
+    }
+
+  console.log("FixBee beginner business description script");
+  console.log("Reading from:", oldCol);
+  console.log("Saving into:", newCol);
+
+
+}

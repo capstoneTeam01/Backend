c75462add64030ad5f8093f206c5a3639b336a82
anmolss-tech <anmolss.tech@gmail.com>
2026-06-28 23:17:49 -0700

    FIXBEE-431-Add-protected-provider-quote-request-route

--- src/routes/businessDirectoryProviderRoutes.js
+++ src/routes/businessDirectoryProviderRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
-import { bdHealth, bdSync } from "../handlers/businessDirectoryProviderHandler.js";
+import { bdHealth, bdSync, sendQuoteRequest } from "../handlers/businessDirectoryProviderHandler.js";
 import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 
 
 const businessDirectoryProviderRoutes = (services = {}) => {
-    console.log("working")
+    console.log("[FixBee][ProviderRoute] business directory provider routes ready")
     const router = express.Router();
 
     router.get("/health", bdHealth);
     router.get("/sync", AuthMiddleware(services), bdSync);
+    router.post("/quote-requests/send", AuthMiddleware(services), sendQuoteRequest);
 
     return router;
 }

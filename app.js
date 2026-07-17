2bb2c3678bb86bad5b8b4e71801c5db4a401221f
barapatikumaraswamy-stack <kbarapati00@mylangara.ca>
2026-07-16 19:21:07 -0700

    Fix landing page folder path

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
+app.use(express.static("public/LandingPage"));
 app.use(express.static("public"));
 
 const services = {};
